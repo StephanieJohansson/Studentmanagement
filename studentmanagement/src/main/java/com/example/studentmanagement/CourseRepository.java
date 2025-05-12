e6dc83f8687c5cf2baeb0c730b7a87918c0da441
StephanieJohansson <3tomu24.johast@folkuniversitetet.nu>
2025-05-12 10:10:34 +0200

    creating two separate repos to student and course

---
+++ studentmanagement/src/main/java/com/example/studentmanagement/CourseRepository.java
@@ -0,0 +1,9 @@
+package com.example.studentmanagement;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CourseRepository extends JpaRepository<Course, Long> {
+    boolean existsByCode(String code);
+}
